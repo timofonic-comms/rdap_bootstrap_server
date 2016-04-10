71fca0f88ae26f77cebd676503f1e96d4a0f84ea
Andrew Newton <andy@arin.net>
2016-04-10 19:39:32 -0400

    initial start on format and source interfaces

---
+++ src/main/java/net/arin/rdap_bootstrap/service/FileSource.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 American Registry for Internet Numbers (ARIN)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package net.arin.rdap_bootstrap.service;
+
+import net.arin.rdap_bootstrap.lookup.Store;
+
+/**
+ * A source type for a file on disk
+ */
+public class FileSource implements Source
+{
+    private Store store;
+    private Format format;
+
+    @Override
+    public void config( String registryName )
+    {
+
+    }
+
+    @Override
+    public void setStore( Store store )
+    {
+        this.store = store;
+    }
+
+    @Override
+    public void setFormat( Format format )
+    {
+        this.format = format;
+    }
+
+    @Override
+    public void execute()
+    {
+
+    }
+}
---
+++ src/main/java/net/arin/rdap_bootstrap/service/Source.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 American Registry for Internet Numbers (ARIN)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package net.arin.rdap_bootstrap.service;
+
+import net.arin.rdap_bootstrap.lookup.Store;
+
+/**
+ * Defines the behavior for a registry source
+ */
+public interface Source
+{
+    /*
+     * Instructs the source to configure itself.
+     */
+    void config( String registryName );
+
+    /*
+     * Sets the pace where the values of the source are to be set.
+     */
+    void setStore( Store store );
+
+    /*
+     * Sets the format of the source
+     */
+    void setFormat( Format format );
+
+    /*
+     * Instructs the source to do what is necessary to load data into the store using the format.
+     * Will only be called after the other methods have been called.
+     * It is also assumed that if the source is to be regularly checked, this method will
+     * initiate any background tasks.
+     */
+    void execute();
+}
